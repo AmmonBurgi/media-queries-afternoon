379c94e22d3dd6b5a21e41f1ec44dd3ffd06c816
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-03-04 22:32:08 -0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      dropDown: false
+    }
+  }
+  handleClick =() =>{
+    this.setState({dropDown: !this.state.dropDown})
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="nav-wrapper">
+        <span className='bootstrap'>Start Bootstrap</span>
+          <div className='menu-initalize'>
+            <span className='drop-menu-logo' onClick={this.handleClick}>
+              <p className='menu'>MENU</p>
+              <p>&#9776;</p>
+            </span>
+          </div>
+
+        <div className='header-initalize'>
+          <div className='header-info'>
+            <span className='serv'>Services</span>
+            <span className='port'>Portfolio</span>
+            <span className='about'>About</span>
+            <span className='team'>Team</span>
+            <span className='contact'>Contact</span>
+          </div>
+        </div>
       </header>
+      <div className={this.state.dropDown ? 'dropdown-info initial-drop-info' : 'dropdown-info' }>
+        <span className='serv-span'>Services</span>
+        <span className='port-span'>Portfolio</span>
+        <span className='about-span'>About</span>
+        <span className='team-span'>Team</span>
+        <span className='contact-span'>Contact</span>
+      </div>
+      {/* <div className='main-body'>
+          <span className='welcome'>Welcome To Our Studio</span>
+          <span className='meet'>IT'S NICE TO MEET YOU</span>
+          <button className='learn-more'>TELL ME MORE</button>
+      </div> */}
     </div>
-  );
+  )
+  }
 }
 
-export default App;
+export default App
